f5c144ab94b6b2cb2050348d1ecc682154400870
theo <tpm001206@gmail.com>
2024-04-04 11:48:19 +0200

    add comments again

--- celsius_fahrenheit.py
+++ celsius_fahrenheit.py
@@ -6,14 +6,17 @@
 """
 
 def fahrenheit_to_celsius(fahrenheit):
+    # convert fahrenheit to celsius
     tempc = (fahrenheit-32)/1.8
     return tempc
 
 def celsius_to_kelvin(celsius):
+    # convert celsius to kelvin
     tempk = celsius + 273.15
     return tempk
 
 def fahrenheit_to_kelvin(fahrenheit):
+    # convert fahrenheit to kelvin
     w = celsius_to_kelvin(fahrenheit_to_celsius(fahrenheit))
     return w
 
