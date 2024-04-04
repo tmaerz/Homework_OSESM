6bdcd089d11365bc23db8e05a33c8c72b6910bd5
theo <tpm001206@gmail.com>
2024-04-04 12:16:48 +0200

    ad test 2

--- test_celsius_fahrenheit.py
+++ test_celsius_fahrenheit.py
@@ -1,1 +1,11 @@
 from celsiusfahrenheit import fahrenheit_to_celsius
+from celsiusfahrenheit import fahrenheit_to_kelvin
+from celsiusfahrenheit import celsius_to_kelvin
+
+
+def test_fahrenheit_to_celsius():
+    assert round(fahrenheit_to_celsius(90), 2) == 32.22
+    assert round(fahrenheit_to_kelvin(90), 2) == 305.37
+    assert round(celsius_to_kelvin(20), 2) == 293.15
+
+
