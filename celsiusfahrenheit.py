f2fefdf775c33d653d8ac10a9cdcd46c5f1683b6
theo <tpm001206@gmail.com>
2024-04-09 14:26:53 +0200

    update comments
    better comments, all comments translated now

--- celsiusfahrenheit.py
+++ celsiusfahrenheit.py
@@ -4,23 +4,23 @@ Anschließend Ausgabe als Fehrenheit, Celsius und Kelvin
 """
 
 def fahrenheit_to_celsius(fahrenheit):
-    # convert fahrenheit to celsius
+    """ convert fahrenheit to celsius"""
     tempc = (fahrenheit-32)/1.8
     return tempc
 
 def celsius_to_kelvin(celsius):
-    # convert celsius to kelvin
+    """ convert celsius to kelvin """
     tempk = celsius + 273.15
     return tempk
 
 def fahrenheit_to_kelvin(fahrenheit):
-    # convert fahrenheit to kelvin
+    """ convert fahrenheit to kelvin """
     w = celsius_to_kelvin(fahrenheit_to_celsius(fahrenheit))
     return w
 
-fahrenheit = 90  #Die Funktion wird für 90 Grad Fahrenheit berechnet, da pytest mit Eingabeaufforderung fehlgeschlagen
-celsius = round(fahrenheit_to_celsius(fahrenheit),2)  #Ausgabe als gerundeter Wert
-kelvin = round(fahrenheit_to_kelvin(fahrenheit),2)  #Ausgabe als gerundeter Wert
+fahrenheit = 90  #here should be a command prompt. Pytest didn't work because of that so i changed it to a fixed value.
+celsius = round(fahrenheit_to_celsius(fahrenheit),2)  #round the result of celsius
+kelvin = round(fahrenheit_to_kelvin(fahrenheit),2)  #round the result of kelvin
 print("Fahrenheit:", fahrenheit)
 print("Celsius:", celsius)
 print("Kelvin:",  kelvin)
